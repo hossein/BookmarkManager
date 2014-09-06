df3c28ef5217f3f4885b8e25eeeede456c5ff0b3
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2014-09-06 11:02:05 +0430

    Introduced FilePreviewHandler

---
+++ PreviewHandlers/FilePreviewHandler.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <QWidget>
+#include <QString>
+#include <QStringList>
+
+/// Preview handler for files. This class associates itself with some extensions and is called
+/// by FileViewManager to fill in a QWidget* preview class that it has created.
+class FilePreviewHandler
+{
+public:
+    FilePreviewHandler() { }
+    virtual ~FilePreviewHandler() { }
+
+    virtual QString GetUniqueName() = 0;
+    virtual QStringList GetSupportedExtensions() = 0;
+
+    /// The created widget must not be bound to this FilePreviewHandler; even if it is,
+    /// it must be able to be deleted by the calling class/container.
+    virtual QWidget* CreateAndFreeWidget(QWidget* parent) = 0;
+
+    /// filePathName must be ABSOLUTE file path on file system and NOT :archive:.
+    /// FileViewManager that uses this class knows this fact.
+    virtual bool ClearAndSetDataToWidget(const QString& filePathName, QWidget* previewWidget) = 0;
+};
