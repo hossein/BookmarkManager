b935e6b62c84c8dfdbd032213929124e25fd9868
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2014-09-09 15:40:53 +0430

    ImagePreviewHandler goes on operation.

--- PreviewHandlers/FilePreviewHandler.cpp
+++ PreviewHandlers/FilePreviewHandler.cpp
@@ -3,8 +3,10 @@
 #include "FileViewManager.h"
 
 #include "PreviewHandlers/LocalHTMLPreviewHandler.h"
+#include "PreviewHandlers/ImagePreviewHandler.h"
 
 void FilePreviewHandler::InstantiateAllKnownFilePreviewHandlersInFileViewManager(FileViewManager* fview)
 {
     fview->AddPreviewHandler(new LocalHTMLPreviewHandler());
+    fview->AddPreviewHandler(new ImagePreviewHandler());
 }
