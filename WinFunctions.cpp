c7c0314bde194bbeeb1800977e119acdedcd4d4c
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2014-09-17 15:01:58 +0330

    The method for friendly exe name was wrong
    Now we use the version info APIs to get the FileDescription version
    field

--- WinFunctions.cpp
+++ WinFunctions.cpp
@@ -37,19 +37,49 @@ QString WinFunctions::GetProgramDisplayName(const QString& exePathName)
     exePathName.toWCharArray(wsFilePathName);
     wsFilePathName[len + 0] = '\0'; //Qt Doesn't put null-terminator there.
 
-    //Note: I didn't call Co/OleInitialize before all callings of this function in this code
-    //      as required by documentation. Is it needed? Is it called by Qt already?
-    SHFILEINFOW sfi = {0};
-    int ret = SHGetFileInfoW((LPCWSTR)wsFilePathName, -1, &sfi, sizeof(sfi), SHGFI_DISPLAYNAME);
-    if (ret == 0) //Error
-        //We do NOT include the '.exe', however the above API call does include it if user has
-        //  configured Windows to always show extensions and the EXE has not set a display name
-        //  in its resources.
-        return QFileInfo(exePathName).completeBaseName();
+    QString failSafeName = QFileInfo(exePathName).completeBaseName(); //We do NOT include the '.exe'
+
+    //http://stackoverflow.com/questions/940707/how-do-i-programatically-get-the-version-of-a-dll-or-exe-file
+    DWORD verHandle = NULL;
+    DWORD verSize = GetFileVersionInfoSize((LPCWSTR)wsFilePathName, &verHandle);
+    if (verSize == 0) //Error
+        return failSafeName;
+
+    LPSTR verData = new char[verSize]; //`char` should be correct, as verSize is in bytes.
+    BOOL success = GetFileVersionInfo((LPCWSTR)wsFilePathName, verHandle, verSize, verData);
+    if (success == 0) //Error
+        return failSafeName;
+
+    //http://msdn.microsoft.com/en-us/library/windows/desktop/ms647464%28v=vs.85%29.aspx
+    struct LANGUAGECODEPAGE
+    {
+        WORD wLanguage;
+        WORD wCodePage;
+    } *lpTranslate;
+    UINT lpTranslateSize;
+    success = VerQueryValue(verData, L"\\VarFileInfo\\Translation",
+                            (LPVOID*)&lpTranslate, &lpTranslateSize);
+
+    //Now we pick JUST THE FIRST LANG! It's either English or not, whatever.
+    //NOTE: We can search user's language in the array, but we don't.
+
+    //We assume this function always succeeds.
+    WCHAR QueryWStr[100];
+    wsprintf(QueryWStr, L"\\StringFileInfo\\%04x%04x\\FileDescription",
+             lpTranslate[0].wLanguage, lpTranslate[0].wCodePage);
+
+    LPWSTR lpBuffer = NULL;
+    UINT size = 0;
+    success = VerQueryValue(verData, QueryWStr, (LPVOID*)&lpBuffer, &size);
+    if (success == 0) //Error
+        return failSafeName;
 
     //Note: QString::toWCharArray produces UTF-16, BUT QString::fromWCharArray assumes input is UCS-2!
     //      QString::fromUtf16 gets input from UTF-16.
-    QString displayName = QString::fromUtf16(sfi.szDisplayName);
+    QString displayName = QString::fromUtf16(lpBuffer, size); //The size is correct.
+
+    delete[] verData;
+
     return displayName;
 }
 
@@ -60,6 +90,8 @@ QPixmap WinFunctions::GetProgramSmallIcon(const QString& exePathName)
     exePathName.toWCharArray(wsFilePathName);
     wsFilePathName[len + 0] = '\0'; //Qt Doesn't put null-terminator there.
 
+    //Note: I didn't call Co/OleInitialize before all callings of this function in this code
+    //      as required by documentation. Is it needed? Is it called by Qt already?
     SHFILEINFOW sfi = {0};
     int ret = SHGetFileInfoW((LPCWSTR)wsFilePathName, -1, &sfi, sizeof(sfi), SHGFI_ICON | SHGFI_SMALLICON);
     if (ret == 0) //Error
