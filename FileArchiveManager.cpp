a9dd4d36e7beac56a9724749cb225c7d345ed1f4
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2014-09-25 22:21:36 +0330

    Moved AddFileToArchive and related functions to FileArchiveManager

--- FileArchiveManager.cpp
+++ FileArchiveManager.cpp
@@ -1,16 +1,115 @@
 #include "FileArchiveManager.h"
 
+#include "Config.h"
 #include "TransactionalFileOperator.h"
+#include "Util.h"
 
+#include <QDir>
+#include <QFile>
+#include <QFileInfo>
+
+//NOTE: In the process of FileArchiveManager'izing, the key phrases to search for are
+//      the `conf->nominal` and `QDir::current` thingies.
 FileArchiveManager::FileArchiveManager(QWidget* dialogParent, Config* conf,
                                        const QString& archiveName, const QString& archiveRoot,
                                        TransactionalFileOperator* filesTransaction)
     : IManager(dialogParent, conf), m_archiveName(archiveName), m_archiveRoot(archiveRoot)
     , filesTransaction(filesTransaction)
 {
+    //TODO: If archive folder doesn't exist, must create it here, not anywhere else.
+
+    m_archiveRoot = QDir(m_archiveRoot).absolutePath(); //Mainly to remove '/' from the end.
 }
 
 FileArchiveManager::~FileArchiveManager()
 {
 
+}
+
+bool FileArchiveManager::AddFileToArchive(const QString& filePathName, bool removeOriginalFile,
+                                          QString& fileArchiveURL)
+{
+    //Check if file is valid.
+    QFileInfo fi(filePathName);
+    if (!fi.exists())
+        return Error("The selected file \"" + filePathName + "\" does not exist!");
+    else if (!fi.isFile())
+        return Error("The path \"" + filePathName + "\" does not point to a valid file!");
+
+    //Decide where should the file be copied.
+    QString fileRelArchiveURL = CalculateFileArchiveURL(filePathName);
+    QString targetFilePathName = GetFullArchivePathForFile(fileRelArchiveURL);
+    //Out param
+    fileArchiveURL = m_archiveName + "/" + fileRelArchiveURL;
+
+    //Create its directory if doesn't exist.
+    QString targetFileDir = QFileInfo(targetFilePathName).absolutePath();
+    QFileInfo tdi(targetFileDir);
+    if (!tdi.exists())
+    {
+        //Can NOT use `canonicalFilePath`, since the directory still doesn't exist, it will just
+        //  return an empty string.
+        if (!filesTransaction->MakePath(".", tdi.absoluteFilePath()))
+            return Error("Could not create the directory for placing the attached file."
+                         "\n\nDirectory: " + tdi.absoluteFilePath());
+    }
+    else if (!tdi.isDir())
+    {
+        return Error("The path for placing the attached file is not a directory!"
+                     "\n\nDirectory: " + tdi.absoluteFilePath());
+    }
+
+    //Copy the file.
+    bool success = filesTransaction->CopyFile(filePathName, targetFilePathName);
+    if (!success)
+        return Error(QString("Could not copy the source file to destination directory!"
+                             "\n\nSource File: %1\nDestination File: %2")
+                             .arg(filePathName, targetFilePathName));
+
+    //Remove the original file.
+    if (removeOriginalFile)
+    {
+        success = filesTransaction->SystemTrashFile(filePathName);
+        //We do NOT return FALSE in case of failure.
+        Error(QString("Could not delete the original file from your filesystem. "
+                      "You should manually delete it yourself.\n\nFile: %1").arg(filePathName));
+    };
+
+    return true;
+}
+
+QString FileArchiveManager::CalculateFileArchiveURL(const QString& fileFullPathName)
+{
+    QFileInfo fi(fileFullPathName);
+
+    int fileNameHash = FileNameHash(fi.fileName());
+    fileNameHash = fileNameHash % 16;
+
+    //Prefix the randomHash with the already calculated fileNameHash.
+    QString prefix = QString::number(fileNameHash, 16).toUpper();
+    QString fileDir = fi.absolutePath();
+    QString randomHash =
+            Util::NonExistentRandomFileNameInDirectory(fileDir, 7, prefix, "." + fi.suffix());
+
+    //Use `prefix` again to put files in different directories.
+    QString fileArchiveURL = prefix + "/" + randomHash;
+    return fileArchiveURL;
+}
+
+QString FileArchiveManager::GetFullArchivePathForFile(const QString& fileArchiveURL)
+{
+    QString path = m_archiveRoot + "/" + fileArchiveURL;
+    return path;
+}
+
+int FileArchiveManager::FileNameHash(const QString& fileNameOnly)
+{
+    //For now just calculates the utf-8 sum of all bytes.
+    QByteArray utf8 = fileNameOnly.toUtf8();
+    int sum = 0;
+
+    for (int i = 0; i < utf8.size(); i++)
+        sum += utf8[i];
+
+    return sum;
 }
--- FileArchiveManager.h
+++ FileArchiveManager.h
@@ -19,4 +19,19 @@ public:
                        const QString& archiveName, const QString& archiveRoot,
                        TransactionalFileOperator* filesTransaction);
     ~FileArchiveManager();
+
+    /// A Files Transaction MUST have been started before calling this function. And TODO: Check this.
+    bool AddFileToArchive(const QString& filePathName, bool removeOriginalFile,
+                          QString& fileArchiveURL);
+
+private:
+    /// Could be called `CreateFileArchiveURL` too.
+    /// Note: This only happens ONCE, and later if file name in archive, or any other property
+    ///       that is used to calculate the hash or anyhting in the FileArchive changes, the file
+    ///       remains in the folder that it always was and doesn't change location.
+    ///       Also, remaining the file extension does NOT change the extension that is used with
+    ///       the file in the FileArchive.
+    QString CalculateFileArchiveURL(const QString& fileFullPathName);
+    QString GetFullArchivePathForFile(const QString& fileArchiveURL);
+    int FileNameHash(const QString& fileNameOnly);
 };
