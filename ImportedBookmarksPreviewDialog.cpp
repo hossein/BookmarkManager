ed99a969df11e74370ea4fc54c0536b69f9fc7cc
Hossein Azadmanesh <mhazadmanesh2009@gmail.com>
2014-11-15 11:27:12 +0330

    Show bookmark icons in ImportedBMs preview dialog

--- ImportedBookmarksPreviewDialog.cpp
+++ ImportedBookmarksPreviewDialog.cpp
@@ -37,10 +37,16 @@ void ImportedBookmarksPreviewDialog::accept()
 
 void ImportedBookmarksPreviewDialog::AddItems()
 {
+    QIcon icon_folder (":/res/import_folder.png");
+    QIcon icon_okay   (":/res/import_ok.png");
+    QIcon icon_similar(":/res/import_duplicate_similar.png");
+    QIcon icon_exact  (":/res/import_duplicate_exact.png");
+
     foreach (const ImportedBookmarkFolder& ibf, elist->ibflist)
     {
         QTreeWidgetItem* twi = new QTreeWidgetItem();
         twi->setText(0, ibf.title);
+        twi->setIcon(0, icon_folder);
         folderItems[ibf.intId] = twi;
 
         if (ibf.parentId <= 0)
@@ -53,6 +59,12 @@ void ImportedBookmarksPreviewDialog::AddItems()
     {
         QTreeWidgetItem* twi = new QTreeWidgetItem();
         twi->setText(0, ib.title);
+        if (ib.Ex_status == ImportedBookmark::S_AnalyzedExactExistent)
+            twi->setIcon(0, icon_exact);
+        else if (ib.Ex_status == ImportedBookmark::S_AnalyzedSimilarExistent)
+            twi->setIcon(0, icon_similar);
+        else if (ib.Ex_status == ImportedBookmark::S_AnalyzedImportOK)
+            twi->setIcon(0, icon_okay);
 
         if (ib.parentId <= 0)
             ui->twBookmarks->addTopLevelItem(twi);
